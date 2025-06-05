612ac480b7bd5b5e3d994a3bf59494fd00853cbf
kaue-souza7 <kauekaue960@gmail.com>
2025-06-05 10:18:17 -0300

    [feat] organization folders

---
+++ dashboard/admin/pagina_professores.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Título da Página</title>
+    <link rel="stylesheet" href="style.css"> <!-- CSS externo -->
+</head>
+<body>
+
+    <?php
+    echo "<h1>Bem-vindo ao meu site!</h1>";
+    ?>
+
+    <p>Esse é um parágrafo HTML.</p>
+
+</body>
+</html>
